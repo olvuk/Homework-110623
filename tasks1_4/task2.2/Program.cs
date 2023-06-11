afa0effd53bd344cf46f9adb2f66bafad315393b
Olga <radenovic-family@yandex.ru>
2023-06-11 18:15:17 +0200

    Выполнена задача, версия 2

---
+++ tasks1_4/task2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿int a0 = 2;
+int b0 = 3;
+int c0 = 7;
+if (a0 > b0 & a0 > c0) Console.WriteLine("max =" + a0); 
+if (b0 > a0 & b0 > c0) Console.WriteLine("max =" + b0); 
+if (c0 > a0 & c0 > b0) Console.WriteLine("max =" + c0); 
+
+int a1 = 44;
+int b1 = 5;
+int c1 = 78;
+if (a1 > b1 & a1 > c1) Console.WriteLine("max =" + a1); 
+if (b1 > a1 & b1 > c1) Console.WriteLine("max =" + b1); 
+if (c1 > a1 & c1 > b1) Console.WriteLine("max =" + c1);
+
+int a2 = 22;
+int b2 = 3;
+int c2 = 9;
+if (a2 > b2 & a2 > c2) Console.WriteLine("max =" + a2); 
+if (b2 > a2 & b2 > c2) Console.WriteLine("max =" + b2); 
+if (c2 > a2 & c2 > b2) Console.WriteLine("max =" + c2); 
