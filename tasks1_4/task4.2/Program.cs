11e8516844dcbd4bd822230d6cd20f5dcb42feb6
Olga <radenovic-family@yandex.ru>
2023-06-11 18:46:15 +0200

    Выполнена задача, версия 2

---
+++ tasks1_4/task4.2/Program.cs
@@ -0,0 +1,13 @@
+﻿int number0 = 5;
+int count0 = 2;
+while (count0 <= number0) {
+if (count0 % 2 == 0) Console.Write(count0 + " ");
+count0 = count0 + 2;}
+
+Console.WriteLine();
+
+int number1 = 8;
+int count1 = 2;
+while (count1 <= number1) {
+if (count1 % 2 == 0) Console.Write(count1 + " ");
+count1 = count1 + 2;}
