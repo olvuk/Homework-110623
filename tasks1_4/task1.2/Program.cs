b44a3828592a01e5fea3c96e72e75d60de0dae3c
Olga <radenovic-family@yandex.ru>
2023-06-11 18:07:08 +0200

    Выполнена задача

--- tasks1_4/task1.2/Program.cs
+++ tasks1_4/task1.2/Program.cs
@@ -1,14 +1,14 @@
 ﻿int a0 = 5;
 int b0 = 7;
 if (a0 > b0) Console.WriteLine("max =" + a0); 
-else Console.WriteLine("max =" + b0);
+if (a0 < b0) Console.WriteLine("max =" + b0); 
 
 int a1 = 2;
 int b1 = 10;
 if (a1 > b1) Console.WriteLine("max =" + a1); 
-else Console.WriteLine("max =" + b1);
+if (a1 < b1) Console.WriteLine("max =" + b1); 
 
 int a2 = -9;
 int b2 = -3;
 if (a2 > b2) Console.WriteLine("max =" + a2); 
-else Console.WriteLine("max =" + b2);
+if (a2 < b2) Console.WriteLine("max =" + b2); 
---
+++ tasks1_4/task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2 & number1 > number3) Console.WriteLine("max =" + number1); 
+if (number2 > number1 & number2 > number3) Console.WriteLine("max =" + number2); 
+if (number3 > number1 & number3 > number2) Console.WriteLine("max =" + number3);
