86a2ad809b7602b0a1cf0b99087f3ef7e847578a
Olga <radenovic-family@yandex.ru>
2023-06-11 18:39:23 +0200

    Выполнена задача

---
+++ tasks1_4/task4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите, пожалуйста, число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= number) {
+if (count % 2 == 0) Console.Write(count + " ");
+count = count + 2;}
