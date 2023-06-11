142199303c9b074ac65c5626070d1953883b9d0d
Olga <radenovic-family@yandex.ru>
2023-06-11 18:25:44 +0200

    Выполнена задача, версия 2

---
+++ tasks1_4/task3.2/Program.cs
@@ -0,0 +1,11 @@
+﻿int number0 = 4;
+if (number0 % 2 == 0) Console.WriteLine("да");
+if (number0 % 2 != 0) Console.WriteLine("нет");
+
+int number1 = -3;
+if (number1 % 2 == 0) Console.WriteLine("да");
+if (number1 % 2 != 0) Console.WriteLine("нет");
+
+int number2 = 7;
+if (number2 % 2 == 0) Console.WriteLine("да");
+if (number2 % 2 != 0) Console.WriteLine("нет");
