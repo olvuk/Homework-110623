087df47253db54e58ce0380b425426edbc1ed6c2
Olga <radenovic-family@yandex.ru>
2023-06-11 18:21:59 +0200

    Выполнена задача

---
+++ tasks1_4/task3/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите, пожалуйста, число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0) Console.WriteLine("да");
+if (number % 2 != 0) Console.WriteLine("нет");
